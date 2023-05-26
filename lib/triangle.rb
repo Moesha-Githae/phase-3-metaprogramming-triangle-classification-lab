a60c7550e49c98ef9ef3c8e6a69a2c8748d28ae3
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-05-26 08:28:02 +0300

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  attr_accessor :len1, :len2,
+  :len3
+
+  def intialize (len1, len2, len3)
+    @len1 = len1
+    @len2 = len2
+    @len3 = len3
+  end
+
+  def kind 
+    if is_triangle?
+      sides = [len1, len2 ,len3]
+      :equilateral
+    elsif sides.uniq.size ==2
+    :isosceles
+    else
+      :scalene
+    else 
+      raise TriangleError
+    end
+  end
+   class TriangleError < StandardError
+   end
+
+   private
+   def is_triangle?
+    sorted = [len1, len2, len3].sort
+    longest_side = sorted.pop
+    longest_side < sorted.sum
+   end
+
+   def triangle_type
+   end
+
+   
 end
